5e4b843de5cd204e8a58cb938f7831f3eaab6881
lvsl <chin@shaftsoft.ru>
2011-05-21 15:45:19 +0600

    Added design doc

---
+++ lib/__init__.py
@@ -0,0 +1,38 @@
+# import public api here
+
+'''
+API is similar to lxml and nltk
+
+flow:
+mystem -> Parser -> Document -> Word -> Lemma -> Grammeme
+
+API TODO:
+Top-level fucntions:
+
+  mystem.parse
+  mystem.fromstring
+  mystem.tosting
+  mystem.sentinize
+  mystem.tokenize
+  mystem.serialize
+
+
+Top-level classes:
+
+  mystem.Parser
+  mystem.Document
+  mystem.Word
+  mystem.Lemma
+  mystem.Grammeme
+
+
+Top-level modules:
+
+  mystem.util
+
+
+Environment variables:
+
+  MYSTEM_PATH
+
+'''
